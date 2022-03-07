c1fb072c70787ed691c0c52b0a2fa9aa0dd0788b
Billions Joel <joelatugonza@gmail.com>
2022-03-07 18:53:29 +0300

    Testing file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-	return <div className="App"> some text </div>;
+  return <div className="App"> some text </div>;
 }
 
 export default App;
