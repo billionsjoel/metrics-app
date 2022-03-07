00b03e3c661fc9d1290c1894f5fda2d9a504274f
Billions Joel <joelatugonza@gmail.com>
2022-03-07 17:53:49 +0300

    Reinstall eslint and remove provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Provider } from 'react-redux';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
