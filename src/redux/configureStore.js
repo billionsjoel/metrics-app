b208201f9aa3b332bf36b7a5e6ffacaf61c85fde
Billions Joel <joelatugonza@gmail.com>
2022-03-08 16:35:35 +0300

    combine reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,7 +3,6 @@ import { logger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import companyReducer from './company/company';
 
-const reducer = combineReducers({ companyReducer });
 
 const store = createStore(reducer, applyMiddleware(thunk, logger));
 
